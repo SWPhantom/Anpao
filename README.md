6a5b0076367888cb109525059cdd2efae5f9b809
Zhenya Frolov <hellcomestotown@hotmail.com>
2021-10-10 19:32:18 -0700

    added manual spatial sync

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Anpao
+https://en.wikipedia.org/wiki/Anpao
+
+Playground of merging 2+ video streams to make Very Good effects.
--- videoInterlacer.py
+++ videoInterlacer.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 import time
+from datetime import datetime
 
-# gets next frame of video. Returns frame if success. Returns None if failure after 10 tries
+N = 60
+# gets next frame of video. Returns frame if success. Returns None if failure after N tries tries
 def getNextFrame(video):
-  blank = 60
+  blank = N
+
   ret, frame = video.read()
   while not ret:
     print(f'{ret} returned')
@@ -16,7 +19,8 @@ def getNextFrame(video):
   
   return frame
 
-
+# Interlaces images row by row.
+# Takes in two images, outputs an image of w*h size with alternating input image pixel rows
 def interlace(imgL, imgR, h, w):
     inter = np.empty((h, w, 3), imgL.dtype)
     inter[0:h:2, :w, :] = imgL[0:h:2, :w, :]
@@ -24,91 +28,113 @@ def interlace(imgL, imgR, h, w):
     print()
     return inter
 
-v1 = cv2.VideoCapture('newL.MP4')
-v2 = cv2.VideoCapture('newR.MP4')
-fps = v1.get(cv2.CAP_PROP_FPS)
 
+inputFile1 = './input/fixedMove1.MP4'
+inputFile2 = './input/fixedMove2.MP4'
+outputFile = './output/' + datetime.now().strftime("%Y-%m-%d-%H:%M:%S") + 'interlace.mp4'
 
-out = cv2.VideoWriter('666interlace10.mp4',cv2.VideoWriter_fourcc('m','p','4','v'), fps, (int(v1.get(3)), int(v1.get(4))))
-frames = 1000
+def main():
+  v1 = cv2.VideoCapture(inputFile1)
+  v2 = cv2.VideoCapture(inputFile2)
+  fps = v1.get(cv2.CAP_PROP_FPS)
 
-frame1a = np.zeros((1920, 1080, 3), np.uint8)
-frame2a = np.zeros((1920, 1080, 3), np.uint8)
+  print(f'writing to {outputFile}')
+  out = cv2.VideoWriter(outputFile, cv2.VideoWriter_fourcc('m','p','4','v'), fps, (int(v1.get(3)), int(v1.get(4))))
+  frames = 1000
 
-# synchronize
-while (v1.isOpened() and v2.isOpened()):
-  # Capture frame-by-frame
-  user = input("l for left, r for right, b for both (or first), c for continue")
-  
-  if user == 'b':
-    frame1a = getNextFrame(v1)
-    frame2a = getNextFrame(v2)
-  elif user == 'l':
-    frame1a = getNextFrame(v1)
-  elif user == 'r':
-    frame2a = getNextFrame(v2)
-  elif user == 'c':
-    break
-  else:
-    break
+  frame1a = np.zeros((1920, 1080, 3), np.uint8)
+  frame2a = np.zeros((1920, 1080, 3), np.uint8)
 
-  if frame1a is None or frame2a is None:
-    break
-
-  print('printing new frames')
-  cv2.imshow('left', frame1a)
-  cv2.imshow('right', frame2a)
-  cv2.waitKey(10)
-
-blank = 0
-# Loop until the end of the video
-while (frames > 0 and v1.isOpened() and v2.isOpened()):
+  # Synchronization
+  # Temporal
+  # Currenly, allows user to advance one or both of the video sources until synchronization is found.
+  # TODO: Add sync metadata file that captures the video sources and the frame offset to skip this manual sync
+  # TODO: Add way to auto-sync, either with jitter matching OR by using a known sync visual signal (LED flash/color pattern match, etc)
+  while (v1.isOpened() and v2.isOpened()):
     # Capture frame-by-frame
-    ret1, frame1a = v1.read()
-    # ret2, frame1b = v1.read()
-
-    ret3, frame2a = v2.read()
-   # ret4, frame2b = v2.read()
-
-    #if(not ret1 and not ret2 and not ret3 and not ret4):
+    user = input("l for left, r for right, b for both (or first), c for continue:")
     
-    if(not ret1 and not ret3):
-      blank += 1
-      if(blank > 10):
-        break
+    if user == 'b':
+      frame1a = getNextFrame(v1)
+      frame2a = getNextFrame(v2)
+    elif user == 'l':
+      frame1a = getNextFrame(v1)
+    elif user == 'r':
+      frame2a = getNextFrame(v2)
+    elif user == 'c':
+      break
+    else:
+      break
 
-    blank = 0
-    #if(not ret1 or not ret2 or not ret3 or not ret4):
-    if(not ret1 or not ret3):
+    if frame1a is None or frame2a is None:
+      break
+
+    print('printing new frames')
+    cv2.imshow('left', frame1a)
+    cv2.imshow('right', frame2a)
+    cv2.waitKey(10)
+
+  cv2.destroyAllWindows()
+
+  # Positional
+  # Here goes the manual alignment of the frames, to keep the center overlapped
+  # Temporary, as alignment should be done per frame
+  horizontalTranslation = 0
+  verticalTranslation = 0
+  while (v1.isOpened() and v2.isOpened()):
+    translate1 = np.float32([[1, 0, horizontalTranslation],[0, 1, verticalTranslation]])
+    translate2 = np.float32([[1, 0, -horizontalTranslation],[0, 1, -verticalTranslation]])
+    inter = interlace(cv2.warpAffine(frame1a, translate1, (frame1a.shape[1], frame1a.shape[0])), cv2.warpAffine(frame2a, translate2, (frame2a.shape[1], frame2a.shape[0])), 1080, 1920)
+    
+    cv2.imshow('alignment', inter)
+    user = input("h for horizonal alignment, v for vertical alignment, b for both new frames. c for continue:")
+    user2 = input("amount:")
+    if user == 'h':
+      horizontalTranslation += int(user2)
+    elif user == 'v':
+      verticalTranslation += int(user2)
+    elif user == 'b':
+      frame1a = getNextFrame(v1)
+      frame2a = getNextFrame(v2)
+    elif user == 'c':
+      break
+    else:
+      print("wrong!")
       continue
 
-    # frames -= 1
+    cv2.waitKey(10)
+    
+
+  # Loop until the end of the video and interlace the images
+  while (v1.isOpened() and v2.isOpened()):
+    translate1 = np.float32([[1, 0, horizontalTranslation],[0, 1, verticalTranslation]])
+    translate2 = np.float32([[1, 0, -horizontalTranslation],[0, 1, -verticalTranslation]])
+
+    frame1a = getNextFrame(v1)
+    frame2a = getNextFrame(v2)
+
+    if frame1a is None or frame2a is None:
+      break
 
     #frame1a = cv2.resize(frame1a, (1920, 1080), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
     #frame1b = cv2.resize(frame1b, (1920, 1080), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
-    #frame2a = cv2.resize(frame2a, (1920, 1080), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)
-    #frame2b = cv2.resize(frame2b, (1920, 1080), fx = 0, fy = 0, interpolation = cv2.INTER_CUBIC)   
 
-    # Display the resulting frame
-    # cv2.imshow('f1', frame1a)
-    # cv2.imshow('f2', frame1b)
+    inter = interlace(cv2.warpAffine(frame1a, translate1, (frame1a.shape[1], frame1a.shape[0])), cv2.warpAffine(frame2a, translate2, (frame2a.shape[1], frame2a.shape[0])), 1080, 1920)
 
-    inter = interlace(frame1a, frame2a, 1080, 1920)
     cv2.imshow("test", inter)
     out.write(inter)
-    #out.write(frame1b)
-    #out.write(frame2a)
-    #out.write(frame2b)
- 
+
     # define q as the exit button
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
- 
-# release the video capture object
-v1.release()
-v2.release()
-out.release()
 
-# Closes all the windows currently opened.
-cv2.destroyAllWindows()
+  # release the video capture object
+  v1.release()
+  v2.release()
+  out.release()
 
+  # Closes all the windows currently opened.
+  cv2.destroyAllWindows()
+
+if __name__=="__main__":
+  main()
